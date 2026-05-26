68dc5c6963c6a17088e599f92e9c008497c6afd3
duzih <duzih@quickwords.local>
2026-05-26 13:14:08 +0800

    速词 PWA v1.0

---
+++ sw.js
@@ -0,0 +1,34 @@
+var CACHE = 'quickwords-v1';
+var FILES = [
+  'index.html',
+  'manifest.json',
+  'data/cet4.js',
+  'data/cet6.js',
+  'data/postgrad.js'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) {
+      return c.addAll(FILES);
+    }).catch(function(){})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).catch(function() { return new Response('Offline', {status:503}); });
+    })
+  );
+});
